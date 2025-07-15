4b2a33091d65d95a5d394147507e6ff6c27dcc5e
EC2 Default User <ec2-user@ip-172-31-34-38.eu-north-1.compute.internal>
2025-07-15 01:46:55 +0000

    adding python file

---
+++ python.py
@@ -0,0 +1,13 @@
+# This program stores and prints student information
+
+# Declare variables
+student_name = "Rahul"
+student_age = 20
+student_branch = "CSE"
+is_enrolled = True
+
+# Print the values
+print("Student Name:", student_name)
+print("Age:", student_age)
+print("Branch:", student_branch)
+print("Enrolled:", is_enrolled)
